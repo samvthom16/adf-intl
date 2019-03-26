b32d4cb9af66410e8886ed6d40cc45ab02826bae
unknown <samvthom16@gmail.com>
2019-03-27 00:10:38 +0530

    Orbit template fix excerpt

--- orbit-query/articles-grid3.php
+++ orbit-query/articles-grid3.php
@@ -1,14 +1,13 @@
 <ul class="article-list three-list group">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-	<li style="background:#fff;">
+	<li style="background:#fff;position:relative;">
       <?php _e( do_shortcode('[orbit_thumbnail_bg size="medium"]') );?>
       <span class="content-type"><?php _e( do_shortcode('[orbit_post_type]') );?></span>
 			<div class="article-content">
-				<h4><?php the_title();?></h4>
-				<p><?php the_excerpt();?></p>
+				<h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+				<div style="max-height:150px;overflow:hidden;"><?php the_excerpt();?></div>
 				<strong class="link">Read more</strong>
 			</div>
-
-	</li>
+  </li>
   <?php endwhile;?>
 </ul>
