be2dba8611125fc9823339ac452d60dc342dc57c
unknown <samvthom16@gmail.com>
2019-03-26 23:55:54 +0530

    Orbit template using the existing grid

---
+++ orbit-query/articles-grid3.php
@@ -0,0 +1,15 @@
+<ul class="article-list three-list group">
+  <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+	<li>
+		<a href="<?php the_permalink();?>" style="height: 391px;">
+			<?php _e( do_shortcode('[orbit_thumbnail]') );?>
+      <span class="content-type"><?php _e( do_shortcode('[orbit_post_type]') );?></span>
+			<div class="article-content">
+				<h4><?php the_title();?></h4>
+				<p><?php the_excerpt();?></p>
+				<strong class="link">Read more</strong>
+			</div>
+		</a>
+	</li>
+  <?php endwhile;?>
+</ul>
