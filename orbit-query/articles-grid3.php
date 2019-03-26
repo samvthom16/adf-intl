74d335876aa4c0364ed68534f9d92469864929a8
unknown <samvthom16@gmail.com>
2019-03-27 00:15:05 +0530

    Orbit template fix

--- orbit-query/articles-grid3.php
+++ orbit-query/articles-grid3.php
@@ -1,13 +1,21 @@
 <ul class="article-list three-list group">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
 	<li style="background:#fff;position:relative;">
-      <?php _e( do_shortcode('[orbit_thumbnail_bg size="medium"]') );?>
+    <a href="<?php the_permalink();?>">
+      <?php
+        global $post_id;
+        $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'medium' );
+        if( is_array( $thumbnail ) ){
+    			echo "<div class='orbit-thumbnail-bg' style='background-image: url(".$thumbnail[0].");'></div>";
+    		}
+      ?>
       <span class="content-type"><?php _e( do_shortcode('[orbit_post_type]') );?></span>
 			<div class="article-content">
-				<h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+				<h4><?php the_title();?></h4>
 				<div style="max-height:150px;overflow:hidden;"><?php the_excerpt();?></div>
 				<strong class="link">Read more</strong>
 			</div>
+    </a>
   </li>
   <?php endwhile;?>
 </ul>
