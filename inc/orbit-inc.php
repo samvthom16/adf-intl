d403388d63b1425a2c73746ea1c1a012114c6f97
unknown <samvthom16@gmail.com>
2019-05-20 20:05:24 +0530

    Daily digest emails

--- inc/orbit-inc.php
+++ inc/orbit-inc.php
@@ -12,5 +12,17 @@ add_filter( 'orbit_post_type_vars', function( $post_types ){
 		'supports'	 => array('title', 'editor', 'author', 'thumbnail')
 	);
 
+	$post_types['daily-digest'] = array(
+		'slug' 		=> 'daily-digest',
+		'labels'	=> array(
+			'name' 			     => 'Daily Digests',
+			'singular_name'  => 'Daily Digest',
+		),
+		'menu_icon'	=> 'dashicons-format-aside',
+		'public'	   => true,
+		'supports'	 => array('title', 'editor', 'author', 'thumbnail')
+	);
+
+
 	return $post_types;
 } );
