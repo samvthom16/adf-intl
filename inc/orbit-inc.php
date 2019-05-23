cc7e1b6222220e52d4c977f0675e1f61987e994e
unknown <samvthom16@gmail.com>
2019-05-23 15:58:40 +0530

    Add alliance-alert as orbit post types

--- inc/orbit-inc.php
+++ inc/orbit-inc.php
@@ -23,6 +23,17 @@ add_filter( 'orbit_post_type_vars', function( $post_types ){
 		'supports'	 => array('title', 'editor', 'author', 'thumbnail')
 	);
 
+	$post_types['alliance-alert'] = array(
+		'slug' 		=> 'alliance-alert',
+		'labels'	=> array(
+			'name' 			     => 'Alliance Alerts',
+			'singular_name'  => 'Alliance Alert',
+		),
+		'menu_icon'	=> 'dashicons-format-aside',
+		'public'	   => true,
+		'supports'	 => array('title', 'editor', 'author', 'thumbnail')
+	);
+
 
 	return $post_types;
 } );
